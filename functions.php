66b181c89e98f6d49afa0dddaaad2b96f043e4f4
Martin Petiot <73895766+MartinPetiot@users.noreply.github.com>
2020-12-04 09:51:43 +0100

    Partie1et2

--- functions.php
+++ functions.php
@@ -1,1 +1,40 @@
-coucou
+<?php
+
+function displayItem1()
+{
+    $prix = 128.28;
+
+    $nom = " € la Pizza Mamamia ";
+
+    $photo = "<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIh5vpy0pA4Gf8HvnrDOOM4lM0oIo4FNim6A&usqp=CAU\" alt=/>";
+echo $prix;
+echo $nom;
+echo $photo;
+}
+
+function displayItem2()
+{
+    $prix1 = 130.28;
+
+    $nom1 = " € la Pizza Aïe Aïe ";
+
+
+    $photo1 = "<img src=\"pizza1.jpg\" alt=/>";
+echo $prix1;
+echo $nom1;
+echo $photo1;
+}
+
+function displayItem3()
+{
+    $prix2 = 140.25;
+
+    $nom2 = "€ la Pizza Muy bonita ";
+
+    $photo2 = "<img src=\"pizza-maison.jpg\" alt=/>";
+    echo $prix2;
+    echo $nom2;
+    echo $photo2;
+}
+
+?>
