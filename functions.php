0bc8f36d89f0102b103c09fbbb2b3c3b6b916ab0
Martin Petiot <73895766+MartinPetiot@users.noreply.github.com>
2020-12-04 15:22:18 +0100

    Etape4_final

--- functions.php
+++ functions.php
@@ -1,40 +1,44 @@
 <?php
 
-function displayItem1()
-{
-    $prix = 128.28;
 
-    $nom = " € la Pizza Mamamia ";
+    $article1 = [
+    $prix = 128.28,
 
-    $photo = "<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIh5vpy0pA4Gf8HvnrDOOM4lM0oIo4FNim6A&usqp=CAU\" alt=/>";
-echo $prix;
-echo $nom;
-echo $photo;
+    $nom = " € la Pizza Mamamia ",
+
+    $photo = "<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIh5vpy0pA4Gf8HvnrDOOM4lM0oIo4FNim6A&usqp=CAU\" alt=/>",
+
+];
+
+$article2 = [
+
+    $prix = 130.28,
+
+    $nom = " € la Pizza Aïe Aïe ",
+
+
+    $photo = "<img src=\"pizza1.jpg\" alt=/>",
+
+];
+
+$article3 = [
+
+    $prix = 140.25,
+
+    $nom = "€ la Pizza Muy bonita ",
+
+    $photo = "<img src=\"pizza-maison.jpg\" alt=/>",
+
+];
+    $catalogue = [$article1,$article2,$article3];
+function displayItem($prix, $nom, $photo){
+    echo $prix;
+    echo $nom;
+    echo $photo;
+}
+    foreach ($catalogue as $article) {
+        displayItem($article[0], $article[1], $article[2]);
 }
 
-function displayItem2()
-{
-    $prix1 = 130.28;
-
-    $nom1 = " € la Pizza Aïe Aïe ";
-
-
-    $photo1 = "<img src=\"pizza1.jpg\" alt=/>";
-echo $prix1;
-echo $nom1;
-echo $photo1;
-}
-
-function displayItem3()
-{
-    $prix2 = 140.25;
-
-    $nom2 = "€ la Pizza Muy bonita ";
-
-    $photo2 = "<img src=\"pizza-maison.jpg\" alt=/>";
-    echo $prix2;
-    echo $nom2;
-    echo $photo2;
-}
 
 ?>
